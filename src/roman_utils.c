98f9efeed2c6ab58acdd7352e21fd4efcdac1344
anonymous <anonymous>
2016-08-04 19:18:16 -0400

    refactored if/else statements into switch

--- src/roman_utils.c
+++ src/roman_utils.c
@@ -3,35 +3,24 @@
 
 int get_value_for(char roman)
 {
-  if (roman == 'I')
-    {
-      return 1;
+  switch(roman) {
+    case 'I':
+       return 1;
+    case 'V':
+       return 5;
+    case 'X':
+       return 10;
+    case 'L':
+       return 50;
+    case 'C':
+       return 100;
+    case 'D':
+       return 500;
+    case 'M':
+       return 1000;
+    default:
+       return 0;
     }
-  else if (roman == 'V')
-    {
-      return 5;
-    }
-  else if (roman == 'X')
-    {
-      return 10;
-    }
-  else if (roman == 'L')
-    {
-      return 50;
-    }
-  else if (roman == 'C')
-    {
-      return 100;
-    }
-  else if (roman == 'D')
-    {
-      return 500;
-    }
-  else if (roman == 'M')
-    {
-      return 1000;
-    }
-  return 0;
 }
 
 void load_values(int * values, char *roman)
