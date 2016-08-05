fe192bd399400ca70595ca206c65cd40ee3fa962
anonymous <anonymous>
2016-08-04 22:13:01 -0400

    partial refactoring (names)

--- src/roman_to_arabic.c
+++ src/roman_to_arabic.c
@@ -2,13 +2,13 @@
 #include <roman_to_arabic.h>
 #include <roman_utils.h>
 
-int roman_to_arabic(char * roman)
+int roman_to_arabic(char * romanNumeral)
 {
-  int romanLen = strlen(roman);
-  int * values = (int *) malloc(romanLen * sizeof(int));
-  load_values(values, roman);
-  int arabic = compute_arabic_from_values(values, romanLen);
-  free (values);
-  return arabic;
+	int romanLen = strlen(romanNumeral);
+	int * arabicAddends = (int *) malloc(romanLen * sizeof(int));
+	load_values(arabicAddends, romanNumeral);
+	int arabic = compute_arabic_from_values(arabicAddends, romanLen);
+	free (arabicAddends);
+	return arabic;
 }
 
