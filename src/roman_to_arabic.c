9b23be20f84ce5a380212e3d9f3fb0864347c468
anonymous <anonymous>
2016-08-04 23:22:28 -0400

    Added -Werror to treat warnings as errors; removed warning/error

--- src/roman_to_arabic.c
+++ src/roman_to_arabic.c
@@ -1,14 +1,17 @@
 #include <stdlib.h>
 #include <roman_to_arabic.h>
 #include <roman_utils.h>
+#include <string.h>
 
 int roman_to_arabic(char * romanNumeral)
 {
 	int numberOfRomanDigits = strlen(romanNumeral);
+
 	int * arabicAddends = (int *) malloc(numberOfRomanDigits * sizeof(int));
 	find_arabic_addends(arabicAddends, romanNumeral);
-	int arabic = compute_arabic_from_addends(arabicAddends, numberOfRomanDigits);
+
+	int arabicValue = compute_arabic_from_addends(arabicAddends, numberOfRomanDigits);
 	free (arabicAddends);
-	return arabic;
+	return arabicValue;
 }
 
