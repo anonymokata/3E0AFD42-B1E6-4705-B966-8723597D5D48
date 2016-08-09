a61fcae0c23c1d2c8fa6df26577276ce4027ffad
anonymous <anonymous>
2016-08-09 18:17:02 -0400

    Test drove 20. Needed to extract a function when a subtle bug was detected by failing 20.

--- src/arabic_to_roman.c
+++ src/arabic_to_roman.c
@@ -2,23 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 #include <arabic_to_roman.h>
+
+
+// TODO - Could I pass arabicValue as an int and return it?
+// TODO - What's a better name than 'process'
+void process(unsigned int *arabicValue, unsigned int incrementalValue, char * romanDigit, char * romanValue);
+void process(unsigned int *arabicValue, unsigned int incrementalValue, char * romanDigit, char * romanValue)
+{
+    if (*arabicValue >= incrementalValue)
+    {
+        strcat(romanValue, romanDigit);
+        *arabicValue -= incrementalValue;
+        process(arabicValue, incrementalValue, romanDigit, romanValue);
+    }
+}
+
+unsigned int incrementalValue[] = {10, 9, 5, 4 };
+char * romanDigit[] = {"X", "IX", "V", "IV"};
+
 void arabic_to_roman (unsigned int arabicValue, char * romanValue)
 {
     romanValue[0] = 0;
 
-    int incrementalValue[] = {10, 9, 5, 4 };
-    char * romanDigit[] = {"X", "IX", "V", "IV"};
-
     for (int ii=0; ii< sizeof(incrementalValue)/sizeof(int); ii++)
     {
-      if (arabicValue >= incrementalValue[ii])
-      {
-          strcat(romanValue, romanDigit[ii]);
-          arabicValue -= incrementalValue[ii];
-      }
+      process (&arabicValue, incrementalValue[ii], romanDigit[ii], romanValue);
     }
 
-    for (int ii=0; ii< arabicValue; ii++)
+    for (unsigned int ii=0; ii< arabicValue; ii++)
     {
         strcat(romanValue, "I");
     }
--- tests/test_roman_numbers.c
+++ tests/test_roman_numbers.c
@@ -11,8 +11,9 @@
 
 START_TEST (to_roman)
 {
-    int arabic_values [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
-    char * expected_roman_values [] = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX"};
+    int arabic_values [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    char * expected_roman_values [] = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX"};
+
     char romanResult[20]; // TODO - 20 is probably a little big. Longest roman numeral in test is "MMMMDCCXCIV"
     for (int ii=0; ii< sizeof(arabic_values)/sizeof(int); ii++)
     {
