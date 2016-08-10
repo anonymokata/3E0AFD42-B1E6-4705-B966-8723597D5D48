aaa926b79841820852cc4819137cee80bb802431
anonymous <anonymous>
2016-08-09 21:17:04 -0400

    improve layout for readability

--- tests/test_roman_numbers.c
+++ tests/test_roman_numbers.c
@@ -10,17 +10,26 @@
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
 // TODO: Make it easier to eyeball the A/R pairs. That might be a big-ish refactor.
-int arabic_values [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 24, 40, 47, 50, 67, 74, 90, 99, 100, 202, 330, 400, 482, 500, 900, 1000, 2001, 3030, 4400, 4894, 4999};
-char * roman_values [] = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXIV", "XL", "XLVII", "L", "LXVII", "LXXIV", "XC", "XCIX", "C","CCII", "CCCXXX", "CD", "CDLXXXII", "CDC", "CM", "M", "MMI", "MMMXXX", "MMMMCD", "MMMMCDCDXCIV", "MMMMCMXCIX"};
+int arabic_values [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+                        16, 17, 18, 19, 20, 24, 40, 47, 50, 67, 74, 90, 99,
+                        100, 202, 330, 400, 482, 500, 900, 1000,
+                        2001, 3030, 4400, 4894, 4999};
+
+char * roman_values [] = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV",
+                           "XVI", "XVII", "XVIII", "XIX", "XX", "XXIV", "XL", "XLVII", "L", "LXVII", "LXXIV", "XC", "XCIX",
+                           "C","CCII", "CCCXXX", "CD", "CDLXXXII", "CDC", "CM", "M", "MMI",
+                           "MMMXXX", "MMMMCD", "MMMMCDCDXCIV", "MMMMCMXCIX"};
 
 START_TEST (to_roman)
 {
     char romanResult[15]; // TODO - 15 is probably a little big. Longest roman numeral in test is "MMMMDCCCXCIV"
+
     for (int ii=0; ii< sizeof(arabic_values)/sizeof(int); ii++)
     {
         arabic_to_roman(arabic_values[ii], romanResult);
+
+        // TODO: can I extract any of the stuff below into a function that reads better?
         ck_assert_msg(strcmp(romanResult, roman_values[ii])==0,
-// TODO: can I extract any of the stuff below into a function that reads better?
             ANSI_COLOR_RED
             "\nconvert_arabic_to_roman(%d) expected %s but was %s\n"
                  ANSI_COLOR_RESET,
@@ -35,9 +44,11 @@ END_TEST
 START_TEST (to_arabic)
 {
      printf("\n");
-     for (int ii=0; ii < sizeof(arabic_values)/sizeof(int); ii++) {
+     for (int ii=0; ii < sizeof(arabic_values)/sizeof(int); ii++)
+     {
          int arabicValue = roman_to_arabic(roman_values[ii]);
-// TODO: Can I make extract any here, as above?
+
+         // TODO: can I extract any of the stuff below into a function that reads better?
          ck_assert_msg(arabicValue == arabic_values[ii],
                  ANSI_COLOR_RED
                  "\nconvert_roman_to_arabic(%s) expected %d but was %d\n"
